4945600af587a0fe89509d9d25e6fd1a63144a77
AkshatBCT <akshat.goel_cs.bct23@gla.ac.in>
2024-01-09 21:29:15 +0530

    done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Akshat
         </a>
       </header>
     </div>
